b30a7427ebacdd64e4df1193d25cfc930309752a
Félix Sipma <gueux@gueux.org>
2012-02-20 21:17:13 +0100

    minor change to xapian.sh

--- xapian.sh
+++ xapian.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export MYENV=~/earthbuilding-env
+export MYENV=~/earthbuilding-env/lib/python2.7
 export LOCALPYTHON=/usr/lib/python2.7
-ln -s $LOCALPYTHON/dist-packages/xapian/__init__.py $MYENV/lib/python2.7/site-packages/xapian.py
-ln -s $LOCALPYTHON/dist-packages/xapian/_xapian.so $MYENV/lib/python2.7/site-packages/
-if [ ! -f $MYENV/lib/python2.7/site-packages/haystack/backends/xapian_backend.py ]
+ln -s $LOCALPYTHON/dist-packages/xapian/__init__.py $MYENV/site-packages/xapian.py
+ln -s $LOCALPYTHON/dist-packages/xapian/_xapian.so $MYENV/site-packages/
+if [ ! -f $MYENV/site-packages/haystack/backends/xapian_backend.py ]
 then
-    ln -s $MYENV/lib/python2.7/site-packages/xapian_backend.py $MYENV/lib/python2.7/site-packages/haystack/backends/
+    ln -s $MYENV/site-packages/xapian_backend.py $MYENV/site-packages/haystack/backends/
 fi
 
