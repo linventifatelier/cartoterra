a19bfa8667c1eaec96dfa68b90421552ce177e22
Félix Sipma <gueux@gueux.org>
2012-02-21 14:35:35 +0100

    THUMBNAIL_DEBUG = DEBUG

--- settings.py
+++ settings.py
@@ -261,6 +261,7 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
+THUMBNAIL_DEBUG = DEBUG
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
