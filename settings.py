002faec484112be0fccfeff7b305cb198a418ec2
Félix Sipma <gueux@gueux.org>
2012-02-22 12:57:11 +0100

    fix Deprecation Warnings

--- settings.py
+++ settings.py
@@ -110,8 +110,8 @@ SECRET_KEY = "fwg$+clkx(a7edc9dca!d(d7zc6b+1!(!6qix*apf6w^vn_u3c"
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = [
-    "django.template.loaders.filesystem.load_template_source",
-    "django.template.loaders.app_directories.load_template_source",
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 ]
 
 MIDDLEWARE_CLASSES = [
