846ec085ff7bc1f5be250ac1ac2df87534bbc5c9
Félix Sipma <gueux@gueux.org>
2012-03-01 11:38:45 +0100

    add gunicorn.conf.py

---
+++ deploy/gunicorn.conf.py
@@ -0,0 +1,14 @@
+import os
+
+def numCPUs():
+    if not hasattr(os, "sysconf"):
+        raise RuntimeError("No sysconf detected.")
+    return os.sysconf("SC_NPROCESSORS_ONLN")
+
+workers = numCPUs() * 2 + 1
+bind = "127.0.0.1:8000"
+pidfile = "/tmp/gunicorn-demo.pid"
+backlog = 2048
+logfile = "/home/linventifatelier/earthbuilding/log/gunicorn_demo.log"
+loglevel = "info"
+timeout=60
