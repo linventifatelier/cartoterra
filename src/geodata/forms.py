b9bbf46a5b7a9ddb961a66eb3bad874798a96f34
Félix Sipma <gueux@gueux.org>
2012-03-08 20:02:26 +0100

    allow users to edit/add/delete geodatas

--- src/geodata/forms.py
+++ src/geodata/forms.py
@@ -4,8 +4,23 @@ from django import forms
 from olwidget.forms import MapModelForm
 from models import EarthGeoDataMeeting, EarthGeoDataPatrimony, \
      EarthGeoDataConstruction
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms.fields import DateField
 
 
 class EarthGeoDataPatrimonyForm(MapModelForm):
     class Meta:
         model = EarthGeoDataPatrimony
+        exclude = ('creator', 'pub_date', )
+
+
+class EarthGeoDataConstructionForm(MapModelForm):
+    class Meta:
+        model = EarthGeoDataConstruction
+        exclude = ('creator', 'pub_date', )
+
+class EarthGeoDataMeetingForm(MapModelForm):
+    class Meta:
+        model = EarthGeoDataMeeting
+        exclude = ('creator', 'pub_date', )
+
--- src/geodata/views.py
+++ src/geodata/views.py
@@ -5,9 +5,16 @@ from django.views.generic.simple import direct_to_template
 from django.utils.translation import ugettext_lazy as _
 from models import EarthGeoDataPatrimony, EarthGeoDataConstruction,\
      EarthGeoDataMeeting
-from forms import EarthGeoDataPatrimonyForm
+from forms import EarthGeoDataPatrimonyForm, EarthGeoDataConstructionForm,\
+     EarthGeoDataMeetingForm
 from olwidget.widgets import InfoMap, Map, InfoLayer
 from sorl.thumbnail import get_thumbnail
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.views.generic import list_detail, create_update
+
 
 
 def _info_builder(geodata):
@@ -43,7 +50,10 @@ def show_patrimony_all(_):
                    {'name': "Patrimonies",
                     'overlay_style': {'fill_color': 'red',}})
     return direct_to_template(_, 'show_patrimony_all.html',
-                              {'geodata': geodata, 'map': map_})
+                              {'geodata': geodata,
+                               'classurl': 'patrimony',
+                               'classname': 'Patrimony',
+                               'map': map_ })
 
 
 def show_construction_all(_):
@@ -53,7 +63,10 @@ def show_construction_all(_):
                    {'name': "Constructions",
                     'overlay_style': {'fill_color': 'blue',}})
     return direct_to_template(_, 'show_construction_all.html',
-                              {'geodata': geodata, 'map': map_})
+                              {'geodata': geodata,
+                               'classurl': 'construction',
+                               'classname': 'Construction',
+                               'map': map_})
 
 
 def show_meeting_all(_):
@@ -63,7 +76,10 @@ def show_meeting_all(_):
                    {'name': "Meetings",
                     'overlay_style': {'fill_color': 'green',}})
     return direct_to_template(_, 'show_meeting_all.html',
-                              {'geodata': geodata, 'map': map_})
+                              {'geodata': geodata,
+                               'classurl': 'meeting',
+                               'classname': 'Meeting',
+                               'map': map_})
 
 
 def show_bigmap(request):
@@ -118,12 +134,134 @@ def show_meeting(request, ident):
                               {'map': map_, 'place': geodata})
 
 
-def add_patrimony(request):
-    if request.method == "POST":
-        form = EarthGeoDataPatrimonyForm(request.POST)
-    else:
-        form = EarthGeoDataPatrimonyForm() # An unbound form
 
-    return direct_to_template(request, 'add_patrimony.html', {
+error_message = _("Please correct the errors below.")
+
+
+@login_required
+def _add_builder(request, geodatamodel, geodatamodelform, geodatatemplate):
+    geodata = geodatamodel(creator = request.user,
+                           pub_date = datetime.now())
+
+    if request.method == "POST":
+        form = geodatamodelform(request.POST, instance = geodata)
+        if form.is_valid():
+            obj = form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 _("Successfully added %(classurl)s \"%(name)s\".") %
+                                 {"classurl": obj.classurl, "name": obj.name,}
+                                 )
+            return HttpResponseRedirect('/%(classurl)s/%(ident)d/' %
+                                        { 'classurl': obj.classurl, 'ident': obj.id })
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 error_message
+                                 )
+    else:
+        form = geodatamodelform(instance = geodata) # An unbound form
+
+    return direct_to_template(request, geodatatemplate, {
         'form': form,
         })
+
+
+@login_required
+def add_patrimony(request):
+    return _add_builder(request, EarthGeoDataPatrimony, EarthGeoDataPatrimonyForm, 'add_patrimony.html')
+
+
+@login_required
+def add_construction(request):
+    return _add_builder(request, EarthGeoDataConstruction, EarthGeoDataConstructionForm, 'add_construction.html')
+
+
+@login_required
+def add_meeting(request):
+    return _add_builder(request, EarthGeoDataMeeting, EarthGeoDataMeetingForm, 'add_meeting.html')
+
+
+
+@login_required
+def _edit_builder(request, geodatamodel, geodatamodelform, geodatatemplate, ident):
+    geodata = get_object_or_404(geodatamodel, pk=ident)
+    if geodata.creator != request.user:
+        raise HttpResponseForbidden()
+
+    if request.method == "POST":
+        form = geodatamodelform(request.POST, instance = geodata)
+        if form.is_valid():
+            obj = form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 _("Successfully edited %(classurl)s \"%(name)s\".") %
+                                 { 'classurl': obj.classurl, "name": obj.name, }
+                                 )
+            return HttpResponseRedirect('/%(classurl)s/%(ident)d/' %
+                                        { 'classurl': obj.classurl,
+                                          'ident': obj.id })
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 error_message
+                                 )
+    else:
+        form = geodatamodelform(instance = geodata) # An unbound form
+
+    return direct_to_template(request, geodatatemplate, {
+        'form': form,
+        'geodata': geodata,
+        })
+
+
+@login_required
+def edit_patrimony(request, ident):
+    return _edit_builder(request, EarthGeoDataPatrimony,
+                         EarthGeoDataPatrimonyForm, 'edit_patrimony.html', ident)
+
+
+
+@login_required
+def edit_construction(request, ident):
+    return _edit_builder(request, EarthGeoDataConstruction,
+                         EarthGeoDataConstructionForm, 'edit_construction.html', ident)
+
+
+@login_required
+def edit_meeting(request, ident):
+    return _edit_builder(request, EarthGeoDataMeeting,
+                         EarthGeoDataMeetingForm, 'edit_meeting.html', ident)
+
+
+
+
+@login_required
+def _delete_builder(request, geodatamodel, geodatatemplate, ident):
+    geodata = get_object_or_404(geodatamodel, pk=ident)
+
+    if geodata.creator != request.user:
+        raise HttpResponseForbidden()
+
+    if request.method == 'POST':
+        geodata.delete()
+        messages.add_message(request, messages.SUCCESS,
+                             _("Successfully deleted %(classurl)s \"%(name)s\".") %
+                             { 'classurl': geodata.classurl, "name": geodata.name, }
+                             )
+        return HttpResponseRedirect('/')
+    else:
+        return direct_to_template(request, geodatatemplate, {
+            'geodata': geodata,
+        })
+
+
+@login_required
+def delete_patrimony(request, ident):
+    return _delete_builder(request, EarthGeoDataPatrimony, 'delete_patrimony.html', ident)
+
+
+@login_required
+def delete_construction(request, ident):
+    return _delete_builder(request, EarthGeoDataConstruction, 'delete_construction.html', ident)
+
+
+@login_required
+def delete_meeting(request, ident):
+    return _delete_builder(request, EarthGeoDataMeeting, 'delete_meeting.html', ident)
