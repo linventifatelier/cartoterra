12369f5fa3ec7c114d59bcafd32cc10a7164b4db
Félix Sipma <gueux@gueux.org>
2012-07-11 18:04:37 +0200

    modify fabfile.py

--- src/fabfile.py
+++ src/fabfile.py
@@ -1,21 +1,27 @@
 # import fabrics API functions
 from fabric.api import *
 
-def prepare_deploy():
-   #local("./manage.py test my_app")
+env.hosts = ['www-cartoterra@gueux.org:22']
+
+def addcommit():
    local("git add -p && git commit")
 
+def push():
+   local("git push")
+
+def prepare_deploy():
+   #local("./manage.py test geodata")
+   local("git add -p && git commit")
+   local("git push")
+
 def deploy():
-   env.hosts =
    code_dir = '/srv/django/myproject'
    with cd(code_dir):
       run("git pull")
-      run("touch app.wsgi")
+      run("touch wsgi.py")
 
 def dev():
    """ Use development server settings """
-   env.hosts = ['www-cartoterra@gueux.org']
-   env['dir'] = '~/cartoterra/'
    code_dir = '~/cartoterra/'
    with cd(code_dir):
       run("git pull")
