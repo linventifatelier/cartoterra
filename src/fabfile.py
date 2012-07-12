54826cb702a7ddab3538a26dc0e04cec0cb50215
Félix Sipma <gueux@gueux.org>
2012-07-12 11:46:53 +0200

    remove useless gunicorn.2.conf.py remove useless xapian.sh (see fabfile.py) update fabfile.py

--- src/fabfile.py
+++ src/fabfile.py
@@ -10,6 +10,12 @@ env.gunicorn_pid = 'gunicorn-cartoterra.pid'
 env.requirements = env.code_dir + "/src/requirements/project.txt"
 env.local_python = 'python2.6'
 
+def test():
+   with settings(warn_only=True):
+      result = local('./manage.py test geodata', capture=True)
+   if result.failed and not confirm("Tests failed. Continue anyway?"):
+      abort("Aborting at user request.")
+
 def add():
    local("git add -p")
 
@@ -19,25 +25,24 @@ def commit():
 def push():
    local("git push")
 
-def run_ls():
-   run('ls -alt',  capture=False)
-
 def init_remote():
-   with cd(code_dir):
+   with cd(env.code_dir):
       run("git clone git@gueux.org:cartoterra.git .")
-   run("virtualenv --distribute %s" % env.virtualenv_dir)
+   run("virtualenv --distribute %s" % env.virtualenv)
    run("source %s/bin/activate && pip install -r %s" %
        (env.virtualenv, env.requirements))
    run("ln -s /usr/lib/%(localpython)s/dist-packages/xapian/__init__.py \
-       %(env)s/lib/%(localpython)s/site-packages/xapian.py && \
+          %(env)s/lib/%(localpython)s/site-packages/xapian.py && \
        ln -s %(env)s/lib/%(localpython)s/site-packages/xapian_backend.py \
-       %(env)s/lib/%(localpython)s/site-packages/haystack/backends/" %
+          %(env)s/lib/%(localpython)s/site-packages/haystack/backends/ && \
+       ln -s /usr/lib/%(localpython)s/dist-packages/xapian/_xapian.so \
+          %(env)s/lib/%(localpython)s/site-packages/" %
        { 'localpython': env.local_python, 'env': env.virtualenv,})
 
 
 
 def prepare_deploy():
-   #local("./manage.py test geodata")
+   #test()
    add()
    commit()
    push()
