bb618272239b775f74b6d1eacd4d156b5d990a29
Félix Sipma <gueux@gueux.org>
2014-01-22 15:37:04 +0100

    optional use of debug_toolbar in settings.development

--- settings/development.py
+++ settings/development.py
@@ -19,3 +19,19 @@ SERVE_MEDIA = DEBUG
 TEMPLATE_DEBUG = DEBUG
 EMAIL_DEBUG = DEBUG
 THUMBNAIL_DEBUG = DEBUG
+
+## debug_toolbar
+#INTERNAL_IPS = ('127.0.0.1',)
+
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    "django_extensions",
+    #"debug_toolbar",
+]
+
+#MIDDLEWARE_CLASSES = settings.MIDDLEWARE_CLASSES + \
+#    ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+#
+#DEBUG_TOOLBAR_CONFIG = {
+#    "INTERCEPT_REDIRECTS": False,
+#}
