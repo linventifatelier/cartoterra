722f032ff8bc8e7ea4d3790a7c50f6d6b04e1bde
Félix Sipma <felix.sipma@no-log.org>
2015-10-19 16:38:43 +0200

    development: mod_spatialite fix

--- settings/development.py
+++ settings/development.py
@@ -20,18 +20,23 @@ TEMPLATE_DEBUG = DEBUG
 EMAIL_DEBUG = DEBUG
 THUMBNAIL_DEBUG = DEBUG
 
-## debug_toolbar
-#INTERNAL_IPS = ('127.0.0.1',)
+# debug_toolbar
+#
+# INTERNAL_IPS = ('127.0.0.1',)
 
 
 INSTALLED_APPS = INSTALLED_APPS + [
     "django_extensions",
-    #"debug_toolbar",
+    # "debug_toolbar",
 ]
 
-#MIDDLEWARE_CLASSES = settings.MIDDLEWARE_CLASSES + \
-#    ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+# MIDDLEWARE_CLASSES = settings.MIDDLEWARE_CLASSES + \
+#     ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 #
-#DEBUG_TOOLBAR_CONFIG = {
-#    "INTERCEPT_REDIRECTS": False,
-#}
+# DEBUG_TOOLBAR_CONFIG = {
+#     "INTERCEPT_REDIRECTS": False,
+# }
+
+# Support mod_spatialite:
+# https://code.djangoproject.com/ticket/23901
+SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
