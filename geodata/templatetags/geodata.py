86da9708d4caf0e7c9643cb5f60adfac3d6bd273
Félix Sipma <felix.sipma@no-log.org>
2015-11-04 14:59:06 +0100

    unesco_chair_logo templatetag

--- geodata/templatetags/geodata.py
+++ geodata/templatetags/geodata.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag
 def world_heritage_logo():
     return '<img alt="world-heritage" width="30" heigth="30" src="%s" onerror="this.src=\'%s\'" />' % (static('img/world_heritage_logo.svg'), static('img/world_heritage_logo.png'))
+
+
+@register.simple_tag
+def unesco_chair_logo():
+    return '<img alt="unesco-chair" width="100" src="%s" />' % (static('img/unesco_chair_logo.png'))
