2cca9f3b32f4aa6889bfb93245c7af9c8f54ebb1
Félix Sipma <felix.sipma@no-log.org>
2016-06-14 15:47:24 +0200

    use pagedown in faq

--- faq/admin.py
+++ faq/admin.py
@@ -1,9 +1,19 @@
 from models import Question
 from django.contrib import admin
-from hvad import admin as hvadadmin
+from hvad.forms import TranslatableModelForm
+from hvad.admin import TranslatableAdmin
+from pagedown.widgets import AdminPagedownWidget
 
 
-class QuestionAdmin(hvadadmin.TranslatableAdmin):
+class QuestionAdminForm(TranslatableModelForm):
+    def __init__(self, *args, **kwargs):
+        super(QuestionAdminForm, self).__init__(*args, **kwargs)
+        self.fields['question_markdown'].widget = AdminPagedownWidget()
+        self.fields['answer_markdown'].widget = AdminPagedownWidget()
+
+
+class QuestionAdmin(TranslatableAdmin):
     """Question administration interface."""
+    form = QuestionAdminForm
 
 admin.site.register(Question, QuestionAdmin)
