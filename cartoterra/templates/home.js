cd37b98b1e2fc8d7933f67e01b34f278a73f1cdf
Félix Sipma <felix.sipma@no-log.org>
2016-06-21 19:30:12 +0200

    use static image for map in home

--- cartoterra/templates/home.js
+++
@@ -1,7 +0,0 @@
-var homemap = L.map('cartoterra-home-map-id', { zoomControl: false }).setView([20, 0], 1);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "© <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors",
-    minZoom: 1,
-    maxZoom: 20
-}).addTo(homemap);
