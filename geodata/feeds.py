8eb79012f965f06b211b1a8e2f5053d4c9cbd5eb
Félix Sipma <felix.sipma@no-log.org>
2016-07-04 18:02:17 -0400

    use DRY in geodata.feeds

--- geodata/feeds.py
+++ geodata/feeds.py
@@ -1,124 +1,77 @@
-#from django.contrib.syndication.views import Feed
 from django.contrib.syndication.views import Feed
 from django.contrib.gis.feeds import GeoRSSFeed
 from django.utils.translation import ugettext_lazy as _
-#from django.contrib.gis.feeds import Feed
 from django.conf import settings
 from geodata.models import Building, Worksite, Event, Stakeholder, EarthGroup
 import itertools
 
 
-class BuildingFeed(Feed):
+class GeodataAbstractFeed(Feed):
+    feed_type = GeoRSSFeed
+
+    def item_geometry(self, item):
+        return item.geometry
+
+    def item_title(self, item):
+        return item.name
+
+    def item_description(self, item):
+        return item.description
+
+    def item_pubdate(self, item):
+        return item.pub_date
+
+
+class BuildingFeed(GeodataAbstractFeed):
     """A feed presenting changes and additions on Building."""
     title = settings.SITE_NAME + " building news."
     link = "/"
     description = _("Updates on changes and additions to buildings of " +
                     settings.SITE_NAME + ".")
 
-    feed_type = GeoRSSFeed
-
-    #def item_extra_kwargs(self, item):
-    #    return {'geometry' : self.item_geometry(item)}
-
-    def item_geometry(self, item):
-        return item.geometry
-
     def items(self):
         return Building.objects.order_by('-pub_date')[:20]
 
-    def item_title(self, item):
-        return item.name
 
-    def item_description(self, item):
-        return item.description
-
-
-class WorksiteFeed(Feed):
+class WorksiteFeed(GeodataAbstractFeed):
     """A feed presenting changes and additions on Worksite."""
     title = _(settings.SITE_NAME + " worksite news.")
     link = "/"
     description = _("Updates on changes and additions to worksites of " +
                     settings.SITE_NAME + ".")
 
-    feed_type = GeoRSSFeed
-
-    #def item_extra_kwargs(self, item):
-    #    return {'geometry' : self.item_geometry(item)}
-
-    def item_geometry(self, item):
-        return item.geometry
-
     def items(self):
         return Worksite.objects.order_by('-pub_date')[:20]
 
-    def item_title(self, item):
-        return item.name
 
-    def item_description(self, item):
-        return item.description
-
-
-class EventFeed(Feed):
+class EventFeed(GeodataAbstractFeed):
     """A feed presenting changes and additions on Event."""
     title = _(settings.SITE_NAME + " event news.")
     link = "/"
     description = _("Updates on changes and additions to events of " +
                     settings.SITE_NAME + ".")
 
-    feed_type = GeoRSSFeed
-
-    #def item_extra_kwargs(self, item):
-    #    return {'geometry' : self.item_geometry(item)}
-
-    def item_geometry(self, item):
-        return item.geometry
-
     def items(self):
         return Event.objects.order_by('-pub_date')[:20]
 
-    def item_title(self, item):
-        return item.name
 
-    def item_description(self, item):
-        return item.description
-
-
-class StakeholderFeed(Feed):
+class StakeholderFeed(GeodataAbstractFeed):
     """A feed presenting changes and additions on Stakeholder."""
     title = _(settings.SITE_NAME + " stakeholder news.")
     link = "/"
     description = _("Updates on changes and additions to stakeholders of " +
                     settings.SITE_NAME + ".")
 
-    feed_type = GeoRSSFeed
-
-    #def item_extra_kwargs(self, item):
-    #    return {'geometry' : self.item_geometry(item)}
-
-    def item_geometry(self, item):
-        return item.geometry
-
     def items(self):
         return Stakeholder.objects.order_by('-pub_date')[:20]
 
-    def item_title(self, item):
-        return item.name
 
-    def item_description(self, item):
-        return item.description
-
-
-class GeodataFeed(Feed):
+class GeodataFeed(GeodataAbstractFeed):
     """A feed presenting changes and additions on cartoterra."""
     title = _(settings.SITE_NAME + " new entries.")
     link = "/"
     description = _("Updates on changes and additions to geodata of " +
                     settings.SITE_NAME + ".")
-
-    feed_type = GeoRSSFeed
-
-    def item_geometry(self, item):
-        return item.geometry
 
     def items(self):
         queryset = list(itertools.chain(
@@ -127,13 +80,7 @@ class GeodataFeed(Feed):
             Event.objects.order_by('-pub_date')[:20],
             Stakeholder.objects.order_by('-pub_date')[:20]
         ))
-        return sorted(queryset, key=lambda x:x.pub_date, reverse=True)[:20]
-
-    def item_title(self, item):
-        return item.name
-
-    def item_description(self, item):
-        return item.description
+        return sorted(queryset, key=lambda x: x.pub_date, reverse=True)[:20]
 
 
 class EarthGroupFeed(Feed):
