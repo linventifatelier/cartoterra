197fbaad103f546cbdbda75fd9f5f3f92c5533de
Félix Sipma <felix.sipma@no-log.org>
2016-07-05 15:09:03 -0400

    add "add geodata" tool to toolbox

--- geodata/templates/geodata/bigmap.js
+++ geodata/templates/geodata/bigmap.js
@@ -1,5 +1,6 @@
 {% extends "geodata/map_base.js" %}
 {% load l10n %}
+{% load i18n %}
 {% load staticfiles %}
 
 {% block cluster_layer %}
@@ -17,17 +18,27 @@ var declusterlayer{{ module }} = new L.layerGroup();
 {% block extra_layers %}
 var datacontrol{{ module }} = '';
 
+function closegeodatacontrol{{ module }} () {
+    $('#{{ module }}_map').removeClass('col-md-9');
+    $('#{{ module }}_toolbox').removeClass('col-md-3');
+    $('#{{ module }}_toolbox').css("display", "none");
+    $('.geodata-toolbox-tool').css("display", "none");
+    datacontrol{{ module }} = '';
+};
+
 function togglegeodatacontrol{{ module }} (control) {
     return function () {
         if (datacontrol{{ module }} == control) {
-            $('#{{ module }}_map').removeClass('col-md-9');
-            $('#{{ module }}_toolbox').removeClass('col-md-3');
-            $('#{{ module }}_toolbox').css("display", "none");
-            datacontrol{{ module }} = '';
-        } else {
+            closegeodatacontrol{{ module }}();
+        } else if (datacontrol{{ module }} == '') {
             $('#{{ module }}_map').addClass('col-md-9');
             $('#{{ module }}_toolbox').addClass('col-md-3');
             $('#{{ module }}_toolbox').css("display", "block");
+            $(control).css("display", "block");
+            datacontrol{{ module }} = control;
+        } else {
+            $('.geodata-toolbox-tool').css("display", "none");
+            $(control).css("display", "block");
             datacontrol{{ module }} = control;
         };
     };
@@ -41,14 +52,20 @@ L.Control.Data{{ module }} = L.Control.extend(
     },
     onAdd: function (map) {
         var controlDiv = L.DomUtil.create('div', 'leaflet-control-toolbar leaflet-bar');
+        var controlDataDiv = L.DomUtil.create('a', 'leaflet-control-data', controlDiv);
+        controlDataDiv.title = '{% trans "Select data to show" %}';
+        controlDataDiv.href = '#';
         L.DomEvent
-            .addListener(controlDiv, 'click', L.DomEvent.stopPropagation)
-            .addListener(controlDiv, 'click', L.DomEvent.preventDefault)
-            .addListener(controlDiv, 'click', togglegeodatacontrol{{ module }}('data'));
-
-        var controlUI = L.DomUtil.create('a', 'leaflet-control-data', controlDiv);
-        controlUI.title = 'Select data to show';
-        controlUI.href = '#';
+            .addListener(controlDataDiv, 'click', L.DomEvent.stopPropagation)
+            .addListener(controlDataDiv, 'click', L.DomEvent.preventDefault)
+            .addListener(controlDataDiv, 'click', togglegeodatacontrol{{ module }}('#{{ module }}_toolbox_filter'));
+        var controlAddDiv = L.DomUtil.create('a', 'leaflet-control-add', controlDiv);
+        controlAddDiv.title = '{% trans "Add a geodata" %}';
+        controlAddDiv.href = '#';
+        L.DomEvent
+            .addListener(controlAddDiv, 'click', L.DomEvent.stopPropagation)
+            .addListener(controlAddDiv, 'click', L.DomEvent.preventDefault)
+            .addListener(controlAddDiv, 'click', togglegeodatacontrol{{ module }}('#{{ module }}_toolbox_add'));
         return controlDiv;
     }
 });
